e8b84277354357914d8d7e3ba7c0dc2ea72b4844
Marty Umlas <martyumlas@gmail.com>
2024-08-09 13:53:52 +0800

    add helmet for http headers

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const db = require('./config/db.js')
 const cors = require('./config/cors.js');
 const bcrypt = require('bcrypt')
 const morgan = require('morgan')
+const helmet = require('helmet')
 
 const { createResponse } = require('./utils/responseUtil');
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors)
 app.use(morgan('combined'))
+app.use(helmet())
 
 
 app.get('/', async (req, res) => {
